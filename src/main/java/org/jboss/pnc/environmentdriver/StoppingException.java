d1e2a2725e9031f2fccb66bd9895a43e9d1e6b1d
Matej Lazar <matejonnet@gmail.com>
2021-05-20 19:57:26 +0200

    Add gracefull shutdown.

---
+++ src/main/java/org/jboss/pnc/environmentdriver/StoppingException.java
@@ -0,0 +1,8 @@
+package org.jboss.pnc.environmentdriver;
+
+/**
+ * @author <a href="mailto:matejonnet@gmail.com">Matej Lazar</a>
+ */
+public class StoppingException extends RuntimeException {
+
+}
